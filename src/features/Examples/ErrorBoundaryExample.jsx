1a72c59de85504d47e2dc30863cae26eaba48d82
DenysenkoStas <49059676+denysenkostas@users.noreply.github.com>
2021-10-24 13:19:17 +0300

    ErrorBoundary fix

--- src/features/Examples/ErrorBoundaryExample.jsx
+++ src/features/Examples/ErrorBoundaryExample.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ErrorBoundary from '../../shared/ErrorBoundary';
+import {ButtonMUI} from '../../shared';
+
+const BuggyCounter = ({title = 'Counter'}) => {
+  const [counter, setCounter] = useState(0);
+  const handleCounter = () => setCounter((prev) => prev + 1);
+
+  if (counter === 3) throw new Error('I crashed!');
+
+  return (
+    <ButtonMUI onClick={handleCounter}>
+      {title}: {counter}
+    </ButtonMUI>
+  );
+};
 
 const ErrorBoundaryExample = () => {
   return (
     <>
       <h2 className='mb-15'>ErrorBoundary</h2>
 
-      <div className='card-wrap flex gap-25'>
-        <ErrorBoundary>{error}</ErrorBoundary>
-      </div>
+      <section className='card-wrap'>
+        <p>
+          <b>Props:</b> children, className, details
+        </p>
+
+        <div className='flex gap-25 mt-25'>
+          <ErrorBoundary>
+            <BuggyCounter />
+          </ErrorBoundary>
+
+          <ErrorBoundary details>
+            <BuggyCounter title='With details' />
+          </ErrorBoundary>
+        </div>
+      </section>
     </>
   );
 };
