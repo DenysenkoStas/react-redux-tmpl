2a14c341b41d1ff603da2123a0c918fa9334f947
DenysenkoStas <49059676+denysenkostas@users.noreply.github.com>
2021-12-11 01:58:46 +0200

    Pagination

--- src/features/Examples/PaginationExample.jsx
+++ src/features/Examples/PaginationExample.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Pagination from '../../shared/Pagination';
 
 const PaginationExample = () => {
+  const [posts, setPosts] = useState([]);
+  const totalPages = 20;
+
+  const getPost = (page = 1) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=5`)
+      .then((res) => res.json())
+      .then((json) => setPosts(json));
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  const handlePageClick = (e) => getPost(e.selected + 1);
+
   return (
     <>
       <h2 className='mb-15'>Pagination</h2>
 
       <section className='card-wrap'>
-        <Pagination pageCount={10} />
+        <p className='mb-25'>
+          <b>Props:</b> active, className, marginPagesDisplayed, onChange, pageCount, pageRangeDisplayed, position
+        </p>
+
+        {posts &&
+          posts.map(({id, title, body}) => (
+            <article key={id} className='demo-article mb-10 p-8'>
+              <h3>
+                <span>{id}. </span>
+                <span className='demo-article__title'>{title}</span>
+              </h3>
+              <p className='demo-article__text'>{body}</p>
+            </article>
+          ))}
+
+        <Pagination pageCount={totalPages} onChange={handlePageClick} />
       </section>
     </>
   );
--- src/shared/Pagination/Pagination.jsx
+++ src/shared/Pagination/Pagination.jsx
@@ -7,40 +7,46 @@ import './Pagination.scss';
 import {ReactComponent as PrevIcon} from './icons/prev.svg';
 import {ReactComponent as NextIcon} from './icons/next.svg';
 
-const Pagination = ({active, pageCount, onChange, count, pageLength}) => {
+const Pagination = ({
+  active,
+  className = '',
+  marginPagesDisplayed = 1,
+  onChange,
+  pageCount,
+  pageRangeDisplayed = 5,
+  position = ''
+}) => {
   return (
-    <div className='pagination-container'>
-      <ReactPaginate
-        forcePage={active}
-        onPageChange={onChange}
-        pageCount={pageCount}
-        pageRangeDisplayed={5}
-        marginPagesDisplayed={1}
-        previousLabel={<PrevIcon />}
-        nextLabel={<NextIcon />}
-        containerClassName='pagination-list'
-        pageClassName='pagination-item'
-        pageLinkClassName='pagination-link'
-        activeLinkClassName='pagination-link-active'
-        breakClassName='pagination-ellipsis'
-      />
-
-      {pageLength && (
-        <span className='pagination-container__msg'>
-          Displaying page {active + 1} of {pageCount}, items {3 * (active + 1) - 2} to {3 * active + pageLength} of{' '}
-          {count}
-        </span>
-      )}
-    </div>
+    <ReactPaginate
+      pageCount={pageCount}
+      pageRangeDisplayed={pageRangeDisplayed}
+      marginPagesDisplayed={marginPagesDisplayed}
+      previousLabel={<PrevIcon className='pagination__icon' />}
+      nextLabel={<NextIcon className='pagination__icon' />}
+      onPageChange={onChange}
+      forcePage={active}
+      containerClassName={`pagination${position && ` pagination--${position}`}${className && ` ${className}`}`}
+      pageClassName='pagination__item'
+      pageLinkClassName='pagination__link'
+      activeLinkClassName='pagination__link--active'
+      breakClassName='pagination__ellipsis'
+      breakLinkClassName='pagination__ellipsis-link'
+      previousClassName='pagination__prev-item'
+      nextClassName='pagination__next-item'
+      previousLinkClassName='pagination__prev-link'
+      nextLinkClassName='pagination__next-link'
+    />
   );
 };
 
 Pagination.propTypes = {
   active: PropTypes.number,
-  count: PropTypes.number,
+  className: PropTypes.string,
+  marginPagesDisplayed: PropTypes.number,
   onChange: PropTypes.func,
   pageCount: PropTypes.number.isRequired,
-  pageLength: PropTypes.number,
+  pageRangeDisplayed: PropTypes.number,
+  position: PropTypes.oneOf(['left', 'right', 'center'])
 };
 
 export default Pagination;
