8b95e865eb6d5c25f850d4468b632b5c61303b56
DenysenkoStas <49059676+DenysenkoStas@users.noreply.github.com>
2022-10-05 14:14:32 +0300

    Fix styles, helpers.scss. New helper functions

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -49,3 +49,24 @@ export function stringToSlug(str) {
 
   return str;
 }
+
+export const generateId = () => Math.floor(Math.random() * Date.now());
+
+export const randomInteger = (max) => Math.floor(Math.random() * (max + 1));
+
+export const randomRgbColor = () => {
+  let r = randomInteger(255);
+  let g = randomInteger(255);
+  let b = randomInteger(255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+export const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
